ba918be85d961a696468f79531e52a17ebcdc417
Aya Kraise <ayak@Ayas-MacBook-Pro.local>
2025-07-20 15:16:18 +0300

    Changed formatter to rfc style

--- flake.nix
+++ flake.nix
@@ -16,142 +16,156 @@
     };
 
   };
-  outputs = inputs@{ self, nix-darwin, nixpkgs, nix-homebrew, homebrew-cask
-    , homebrew-core }:
+  outputs =
+    inputs@{
+      self,
+      nix-darwin,
+      nixpkgs,
+      nix-homebrew,
+      homebrew-cask,
+      homebrew-core,
+    }:
     let
-      configuration = { pkgs, config, ... }: {
-        # List packages installed in system profile. To search by name, run:
-        # $ nix-env -qaP | grep wget
-        environment.systemPackages = [
-          pkgs.neovim
-          pkgs.yabai
-          pkgs.nixfmt
-          pkgs.ngrok
-          pkgs.nodejs
-          pkgs.dotnet-sdk
-          pkgs.python3
-          pkgs.skhd
-          pkgs.jetbrains-toolbox
-          # pkgs.sketchybar
-          pkgs.betterdisplay
-          pkgs.slack
-          pkgs.spotify
-          pkgs.vscode
-          pkgs.windsurf
-          pkgs.notion-app
-          pkgs.code-cursor
-          pkgs.mkalias
-          pkgs.alacritty
-          pkgs.zoom-us
-        ];
-
-        fonts.packages = [
-          pkgs.maple-mono.NF-unhinted
-
-        ];
-
-        homebrew = {
-          enable = true;
-          masApps = {
-            "Word" = 462054704;
-            "Excel" = 462058435;
-            "Whatsapp-messanger" = 310633997;
-          };
-          onActivation.cleanup = "zap";
-          onActivation.autoUpdate = true;
-          onActivation.upgrade = true;
-        };
-        system.primaryUser = "ayak";
-        system.activationScripts.applications.text = let
-          env = pkgs.buildEnv {
-            name = "system-applications";
-            paths = config.environment.systemPackages;
-            pathsToLink = "/Applications";
-          };
-        in pkgs.lib.mkForce ''
-          # Set up applications.
-          echo "setting up /Applications..." >&2
-          rm -rf /Applications/Nix\ Apps
-          mkdir -p /Applications/Nix\ Apps
-          find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
-          while read -r src; do
-            app_name=$(basename "$src")
-            echo "copying $src" >&2
-            ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
-          done
-        '';
-        system.defaults = {
-          dock.autohide = true;
-          dock.persistent-apps = [
-            "/System/Applications/Launchpad.app"
-            "/System/Cryptexes/App/System/Applications/Safari.app"
-            "/System/Applications/Mail.app"
-            "/System/Applications/Calendar.app"
-            "/Users/ayak/Applications/Rider.app"
-            "/Users/ayak/Applications/PyCharm.app"
-            "${pkgs.alacritty}/Applications/Alacritty.app"
+      configuration =
+        { pkgs, config, ... }:
+        {
+          # List packages installed in system profile. To search by name, run:
+          # $ nix-env -qaP | grep wget
+          environment.systemPackages = [
+            pkgs.neovim
+            pkgs.yabai
+            pkgs.nixfmt-rfc-style
+            pkgs.ngrok
+            pkgs.nodejs
+            pkgs.dotnet-sdk
+            pkgs.python3
+            pkgs.skhd
+            pkgs.jetbrains-toolbox
+            # pkgs.sketchybar
+            pkgs.betterdisplay
+            pkgs.slack
+            pkgs.spotify
+            pkgs.vscode
+            pkgs.windsurf
+            pkgs.notion-app
+            pkgs.code-cursor
+            pkgs.mkalias
+            pkgs.alacritty
+            pkgs.zoom-us
           ];
-          NSGlobalDomain = {
-            AppleShowAllExtensions = true;
-            AppleShowAllFiles = true;
-            # _HIHideMenuBar = true;
-            KeyRepeat = 2;
-            ApplePressAndHoldEnabled = false;
-          };
-          screencapture.location = "~/Pictures/Screenshots";
 
-        };
+          fonts.packages = [
+            pkgs.maple-mono.NF-unhinted
 
-        nix.enable = false;
-        nixpkgs.config = { allowUnfree = true; };
-        services = {
-          yabai.enable = true;
-          yabai.config = {
-            focus_follows_mouse = "autoraise";
-            mouse_follows_focus = "on";
-            top_padding = 20;
-            bottom_padding = 20;
-            right_padding = 20;
-            left_padding = 20;
-            window_gap = 20;
-            # external_bar = "all:20:0";
-            layout = "bsp";
-            window_placement = "second_child";
-            mouse_modifier = "alt";
-            mouse_action1 = "move";
-            mouse_action2 = "resize";
-          };
+          ];
 
-          skhd = {
+          homebrew = {
             enable = true;
-            skhdConfig = builtins.readFile ./skhdrc;
+            masApps = {
+              "Word" = 462054704;
+              "Excel" = 462058435;
+              "Whatsapp-messanger" = 310633997;
+            };
+            onActivation.cleanup = "zap";
+            onActivation.autoUpdate = true;
+            onActivation.upgrade = true;
+          };
+          system.primaryUser = "ayak";
+          system.activationScripts.applications.text =
+            let
+              env = pkgs.buildEnv {
+                name = "system-applications";
+                paths = config.environment.systemPackages;
+                pathsToLink = "/Applications";
+              };
+            in
+            pkgs.lib.mkForce ''
+              # Set up applications.
+              echo "setting up /Applications..." >&2
+              rm -rf /Applications/Nix\ Apps
+              mkdir -p /Applications/Nix\ Apps
+              find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
+              while read -r src; do
+                app_name=$(basename "$src")
+                echo "copying $src" >&2
+                ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
+              done
+            '';
+          system.defaults = {
+            dock.autohide = true;
+            dock.persistent-apps = [
+              "/System/Applications/Launchpad.app"
+              "/System/Cryptexes/App/System/Applications/Safari.app"
+              "/System/Applications/Mail.app"
+              "/System/Applications/Calendar.app"
+              "/Users/ayak/Applications/Rider.app"
+              "/Users/ayak/Applications/PyCharm.app"
+              "${pkgs.alacritty}/Applications/Alacritty.app"
+            ];
+            NSGlobalDomain = {
+              AppleShowAllExtensions = true;
+              AppleShowAllFiles = true;
+              # _HIHideMenuBar = true;
+              KeyRepeat = 2;
+              ApplePressAndHoldEnabled = false;
+            };
+            screencapture.location = "~/Pictures/Screenshots";
+
           };
 
-          # sketchybar = {
-          #         enable = true;
-          #         config = builtins.readFile ./sketchybarrc;
-          #     };
+          nix.enable = false;
+          nixpkgs.config = {
+            allowUnfree = true;
+          };
+          services = {
+            yabai.enable = true;
+            yabai.config = {
+              focus_follows_mouse = "autoraise";
+              mouse_follows_focus = "on";
+              top_padding = 20;
+              bottom_padding = 20;
+              right_padding = 20;
+              left_padding = 20;
+              window_gap = 20;
+              # external_bar = "all:20:0";
+              layout = "bsp";
+              window_placement = "second_child";
+              mouse_modifier = "alt";
+              mouse_action1 = "move";
+              mouse_action2 = "resize";
+            };
+
+            skhd = {
+              enable = true;
+            skhdConfig = builtins.readFile ./skhdrc;
+            };
+
+            # sketchybar = {
+            #         enable = true;
+            #         config = builtins.readFile ./sketchybarrc;
+            #     };
+          };
+
+          security.pam.services.sudo_local.touchIdAuth = true;
+
+          # Necessary for using flakes on this system.
+          nix.settings.experimental-features = "nix-command flakes";
+
+          # Enable alternative shell support in nix-darwin.
+          # programs.fish.enable = true;
+
+          # Set Git commit hash for darwin-version.
+          system.configurationRevision = self.rev or self.dirtyRev or null;
+
+          # Used for backwards compatibility, please read the changelog before changing.
+          # $ darwin-rebuild changelog
+          system.stateVersion = 6;
+
+          # The platform the configuration will be used on.
+          nixpkgs.hostPlatform = "aarch64-darwin";
         };
-
-        security.pam.services.sudo_local.touchIdAuth = true;
-
-        # Necessary for using flakes on this system.
-        nix.settings.experimental-features = "nix-command flakes";
-
-        # Enable alternative shell support in nix-darwin.
-        # programs.fish.enable = true;
-
-        # Set Git commit hash for darwin-version.
-        system.configurationRevision = self.rev or self.dirtyRev or null;
-
-        # Used for backwards compatibility, please read the changelog before changing.
-        # $ darwin-rebuild changelog
-        system.stateVersion = 6;
-
-        # The platform the configuration will be used on.
-        nixpkgs.hostPlatform = "aarch64-darwin";
-      };
-    in {
+    in
+    {
       # Build darwin flake using:
       # $ darwin-rebuild build --flake .#Ayas-MacBook-Pro
       darwinConfigurations."Ayas-MacBook-Pro" = nix-darwin.lib.darwinSystem {
