14dd43966d2c662d32354e82cb391409f4affb0b
Aya Kraise <aya@ask-y.ai>
2025-10-20 11:56:28 +0300

    Update flake.lock and disable Rosetta installation in flake.nix

--- flake.nix
+++ flake.nix
@@ -231,7 +231,7 @@
               enable = true;
 
               # Apple Silicon Only: Also install Homebrew under the default Intel prefix for Rosetta 2
-              enableRosetta = true;
+              # enableRosetta = true;
 
               # User owning the Homebrew prefix
               user = "ayak";
