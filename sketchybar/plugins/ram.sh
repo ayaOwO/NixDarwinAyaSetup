de60c2c22915d2bacaa92d0c169c228f057c32be
Aya Kraise <aya@ask-y.ai>
2026-03-01 18:42:11 +0200

    Moved to multi line and updated icon

--- sketchybar/plugins/ram.sh
+++ sketchybar/plugins/ram.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 # ram.sh — show system memory used percentage in sketchybar
 
-sketchybar -m --set "$NAME" label="$(memory_pressure | grep "System-wide memory free percentage:" | awk '{ gsub(/%/,"",$5); printf("%02.0f", 100-$5) }')%"
+RAM_ICON=''
+pct=$(memory_pressure | grep "System-wide memory free percentage:" | awk '{ gsub(/%/,"",$5); printf("%02.0f", 100-$5) }')
+sketchybar -m --set "$NAME" icon="$RAM_ICON" label="${pct}%"
